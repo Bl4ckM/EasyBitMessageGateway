81b19c6a794d351a164a02c59b094133658aecfa
UnknownBird <I.fucked.up@yourprojects>
2015-05-23 13:50:40 +0200

    update license

--- src/main/java/com/bl4ckbird/ebitm/main.java
+++ src/main/java/com/bl4ckbird/ebitm/main.java
@@ -1,7 +1,7 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2015 bl4ckbird
+ * Copyright (c) 2015 Lukas Kaupp 'Bl4ckM' <lukas.kaupp@stud.h-da.de>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
